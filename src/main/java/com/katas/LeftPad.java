e3456555b30e4bfa5f2d59b1686d5e6378ee3445
i-katas <feexonj@gmail.com>
2020-02-21 10:03:16 +0800

    make the test pass by fill padding

--- src/main/java/com/katas/LeftPad.java
+++ src/main/java/com/katas/LeftPad.java
@@ -3,6 +3,9 @@ package com.katas;
 public class LeftPad {
   public static String pad(String s, char pad, int n) {
     char[] padding = new char[n];
+    if(n > 0) {
+      padding[0] = pad;
+    }
     return String.valueOf(padding);
   }
 }
