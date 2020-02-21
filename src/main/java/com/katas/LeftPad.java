b9fabbc303301e2c295984ccc7c3273fd04da5dc
i-katas <feexonj@gmail.com>
2020-02-21 10:01:12 +0800

    Introduce a local variable `padding` to save padding

--- src/main/java/com/katas/LeftPad.java
+++ src/main/java/com/katas/LeftPad.java
@@ -1,7 +1,8 @@
 package com.katas;
 
 public class LeftPad {
-  public static String pad(String s, char padding, int n) {
-    return "";
+  public static String pad(String s, char pad, int n) {
+    char[] padding = new char[n];
+    return String.valueOf(padding);
   }
 }
