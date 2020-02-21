bcaf9b42b792a3f3c0a1318cb7e024a2c6f8e160
i-katas <feexonj@gmail.com>
2020-02-21 10:22:55 +0800

    reduce String & char[] creation

--- src/main/java/com/katas/LeftPad.java
+++ src/main/java/com/katas/LeftPad.java
@@ -4,8 +4,12 @@ import java.util.Arrays;
 
 public class LeftPad {
   public static String pad(String s, char pad, int n) {
-    char[] padding = new char[Math.max(0, n - s.length())];
-    Arrays.fill(padding, pad);
-    return String.valueOf(padding) + s;
+    int numPads = Math.max(0, n - s.length());
+    char[] buffer = new char[s.length() + numPads];
+
+    Arrays.fill(buffer, 0, numPads, pad);
+    System.arraycopy(s.toCharArray(), 0, buffer, numPads, s.length());
+
+    return String.valueOf(buffer);
   }
 }
